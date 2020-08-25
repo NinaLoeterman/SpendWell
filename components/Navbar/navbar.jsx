0f45b0e8fa2552659caca5996e5b1530ce1213b4
Nina Loeterman <nina.loeterman@gmail.com>
2020-08-25 13:30:04 +0300

    Icon added but need to restart

--- components/Navbar/navbar.jsx
+++ components/Navbar/navbar.jsx
@@ -1,22 +1,25 @@
 import { useEffect, useState } from "react";
 import React from "react";
 import { AppRegistry, View, Text, Image, StyleSheet } from "react-native";
+import Icon from "react-native-vector-icons/dist/FontAwesome";
 
 const Navbar = () => {
-    return ( <View style={{flex: 1}}>
-        <Image
+  return (
+    <View style={{ flex: 1 }}>
+      <Image
         style={styles.image}
-        source={require('../../pics/SpendWell.png')}
+        source={require("../../pics/SpendWell.png")}
       />
-    </View> );
-}
- 
+      <Icon name="rocket" size={30} color="#900" />{" "}
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
-    image: {
-        width: 150,
-        height: 100,
-    },
-  });
-  
+  image: {
+    width: 150,
+    height: 100,
+  },
+});
 
 export default Navbar;
