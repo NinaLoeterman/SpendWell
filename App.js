c9f7a49f771c955bd033a9716f8d3b5617de736f
Nina Loeterman <nina.loeterman@gmail.com>
2020-08-26 10:46:49 +0300

    routers from login to mainpage working

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
-import * as React from "react";
+// import * as React from "react";
 import { View } from "react-native";
 import { StyleSheet, Text } from "react-native";
 import MainPage from "./components/MainPage/MainPage.jsx";
+import LoginPage from "./components/LoginPage/LoginPage.jsx";
+import { NativeRouter, Route, Link } from "react-router-native";
+import "react-native-gesture-handler";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <MainPage />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Login" component={LoginPage} />
+        <Stack.Screen name="MainPage" component={MainPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    // <View style={styles.container}>
+    //   {/* <MainPage /> */}
+    // </View>
   );
 }
 
